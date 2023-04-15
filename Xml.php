7558533c34e8ac194f0b8bd3b3d772825866e153
yadav-sushant <95368382+yadav-sushant@users.noreply.github.com>
2023-04-15 12:33:44 +0530

    Create Xml.php
    Created library to convert array to XML using PHP(Codeigniter).

---
+++ Xml.php
@@ -0,0 +1,45 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+   
+class Xml {
+    function __construct(){
+    
+    }
+    
+    function arrayToXml($data, $object){
+        foreach ($data as $key => $value) {
+            $key_arr = explode('_',$key);
+            $keyname = (count($key_arr)>1 && is_numeric($key_arr[1]))?$key_arr[0]:$key;
+            $attr = $value["attr"]??[];
+            $value = $value["val"]??[];
+            if(!empty($attr)){
+                foreach($attr as $k => $v) $object->addAttribute($k, $v);
+            }
+            if (is_array($value)) {
+                $new_object = $object->addChild($keyname);
+                $this->arrayToXml($value, $new_object);
+            } 
+            else $object->addChild($keyname, $value);
+        }
+    }
+
+    function export($data = []){
+        if(!empty($data)){
+            $xml = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8" ?><root></root>');
+            $this->arrayToXml($data,$xml);
+            foreach ($xml->xpath('/root/child::'.key($data).'[1]') as $node) {
+                $unwrappedXml = new SimpleXMLElement($node->asXML());
+                break;
+            }
+            
+            $str = $unwrappedXml->asXML();
+            header('Content-Disposition: attachment; filename="tally'.time().'.xml"');
+            header('Content-type: application/xml');
+            header('Expires: 0');
+            header('Cache-Control: must-revalidate');
+            header('Pragma: public');
+            header('Content-Length: ' . strlen($str));
+            header('Connection: close');
+            echo $str;   
+        }
+    }
+}
